46f4bf70d7311cfed723e12099d861c3699b4580
blessystephen0911-dotcom <blessystephen0911@gmail.com>
2025-09-26 11:04:24 +0530

    Update bubble.c

--- bubble.c
+++ bubble.c
@@ -1,33 +1,33 @@
 #include<stdio.h>
-void binary(int a[], int n,int e)
-{int f=0,  t=0,b=n-1,m;
-while(t<=b)
-{m=(t+b)/2;
-if(e==a[m])
-{ f=1;
-  break;}
-else
-  if(e<a[m])
-   b=m-1;
-  else
-   t=m+1;}
-if(f==1)
-printf("Element found at position %d",m+1);
-else 
-  printf("Element not found");
+void bubblesort(int a[],int n)
+{ int p,c,t;
+for( p=1;p<n;p++)
+  for(c=0;c<n-p;c++)
+    {
+     if (a[c]>a[c+1])
+      {t=a[c];
+       a[c]=a[c+1];
+       a[c+1]=t;}
+}
+}
+void printsort(int a[],int n)
+{int i;
+printf("\nSORTED ARRAY");
+for(i=0;i<n;i++)
+ printf("\n%d",a[i]);
+
 }
 
 
 void main()
-{int n,e,i;
+{int n,i,t;
 printf("Enter limit:");
 scanf("%d",&n);
 printf("Enter elements:");
 int a[n];
 for(i=0;i<n;i++)
-scanf("%d",&a[i]);
-printf("Enter element to be found:");
-scanf("%d",&e);
-binary(a,n,e);
-} 
+ scanf("%d",&a[i]);
+bubblesort(a,n);
+printsort(a,n);
+}
  
